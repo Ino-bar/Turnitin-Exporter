bab4e98d4a3050ceec56021bb99d53ac772cec70
ino-bar <50051423+ino-bar@users.noreply.github.com>
2020-02-05 15:14:02 +0000

    05-02-20 created project
    Rushed through this to get something together that works ASAP.

--- Turnitin Exporter/Form1.cs
+++ Turnitin Exporter/Form1.cs
@@ -23,8 +23,13 @@ namespace Turnitin_Exporter
             var FilesToParse = ChooseFolder();
             Directory FilesInFolder = new Directory();
             FilesInFolder.SetFilesInFolderToDirectory(FilesToParse);
+            FilesText filesText = new FilesText();
             PDFParser pdfParser = new PDFParser();
-            Console.WriteLine(pdfParser.ExtractTextFromPdf(FilesInFolder.files[0]));
+            for(int i = 0; i < FilesToParse.Length; i++)
+            { 
+            filesText.SetTextFromFiles(pdfParser.ExtractTextFromPdf(FilesInFolder.files[i]));
+            filesText.GetStudentIDAndFeedback();
+            }
         }
 
         public string[] ChooseFolder()
@@ -55,11 +60,46 @@ namespace Turnitin_Exporter
 
             PdfDocument pdfDoc = new PdfDocument(new PdfReader(path));
                 StringBuilder text = new StringBuilder();
+            if (pdfDoc.GetNumberOfPages() > 0)
+            {
+                //FilesText filesText = new FilesText();
                 for (int i = 1; i <= pdfDoc.GetNumberOfPages(); i++)
                 {
                     text.Append(iText.Kernel.Pdf.Canvas.Parser.PdfTextExtractor.GetTextFromPage(pdfDoc.GetPage(i)));
                 }
                 return text.ToString();
+            }
+            else return null;
+        }
+    }
+    public class FilesText
+    {
+        public string teststring { get; set; }
+        public List<string> textFromFiles = new List<string>();
+        public List<string> ID = new List<string>();
+        public List<string> Feedback = new List<string>();
+
+        public void SetTextFromFiles(string files)
+        {
+            teststring = files;
+            textFromFiles.Add(teststring);
+        }
+        public void GetStudentIDAndFeedback()
+        {
+            for(int i = 0; i < textFromFiles.Count; i++)
+            {
+                
+                int IDFrom = textFromFiles[i].IndexOf("SUBMISSION ID ") + "SUBMISSION ID ".Length;
+                int IDTo = textFromFiles[i].LastIndexOf(" CHARACTER");
+                ID.Add(textFromFiles[i].Substring(IDFrom, IDTo - IDFrom));
+                Console.WriteLine(ID[i]);
+                
+                int stringFrom = textFromFiles[i].IndexOf("Instructor") + "Instructor".Length;
+                int stringTo = textFromFiles[i].LastIndexOf("PAGE 1");
+                Feedback.Add(textFromFiles[i].Substring(stringFrom, stringTo - stringFrom));
+                Console.WriteLine(Feedback[i]);
+                //return result[i];
+            }
         }
     }
 }
