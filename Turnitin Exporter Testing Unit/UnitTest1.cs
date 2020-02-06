d019323d0940f8ddd52eee4a86225229b6a083fa
ino-bar <ino-bar@users.noreply.github.com>
2020-02-06 15:29:44 +0000

    06-02-20 various additions
    Added unit tests, added text class so various versions of the text from the parser can be stored, worked on, printed out, etc.

--- Turnitin Exporter Testing Unit/UnitTest1.cs
+++ Turnitin Exporter Testing Unit/UnitTest1.cs
@@ -2,25 +2,80 @@ using System;
 using Xunit;
 using Turnitin_Exporter;
 using System.Windows.Forms;
+using System.Linq;
+using iText.Kernel.Pdf;
+using System.Text;
+using System.Collections.Generic;
 
 namespace Turnitin_Exporter_Testing_Unit
 {
     public class UnitTest1
     {
+        string[] filesInFolder { get; set; }
         [Fact]
-        public void Test1()
+        public string[] getArrayOfPdfFilesInFolder()
+        {
+            filesInFolder = System.IO.Directory.GetFiles("Z:\\chemistry\\2019-20\\Blackboard\\material chemistry essays", "*.pdf");
+            //Assert.Equal(75, filesInFolder.Length);
+            Assert.NotEmpty(System.IO.Directory.GetFiles("Z:\\chemistry\\2019-20\\Blackboard\\material chemistry essays", "*.pdf"));
+            return System.IO.Directory.GetFiles("Z:\\chemistry\\2019-20\\Blackboard\\material chemistry essays", "*.pdf");
+        }
+        [Fact]
+        public void checkIfTextFileInFolder()
+        {
+            Assert.Empty(System.IO.Directory.GetFiles("Z:\\chemistry\\2019-20\\Blackboard\\material chemistry essays", "*.txt"));
+        }
+        [Fact]
+        public void TestUnit1()
+        {
+            var files = getArrayOfPdfFilesInFolder();
+            Directory FilesInFolder = new Directory();
+            FilesInFolder.SetFilesInFolderToDirectory(files);
+            Assert.Equal(FilesInFolder.files, files);
+        }
+    }
+    public class Directory
+    {
+        public string[] files { get; set; }
+        public void SetFilesInFolderToDirectory(string[] filesToParse)
+        {
+            files = filesToParse;
+        }
+    }
+    public class PDFParser
+    {
+        [Theory]
+        [InlineData("Z:\\chemistry\\2019-20\\Blackboard\\material chemistry essays\\01180527.pdf")]
+        [InlineData("Z:\\chemistry\\2019-20\\Blackboard\\material chemistry essays\\01199862 materials coursework essay.pdf")]
+        public string ExtractTextFromPdf(string path)
         {
 
-        }
-        private void ChooseFolder()
-        {
-            FolderBrowserDialog ChooseFilesFolder = new FolderBrowserDialog();
-            DialogResult result = ChooseFilesFolder.ShowDialog();
-            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(ChooseFilesFolder.SelectedPath))
+            PdfDocument pdfDoc = new PdfDocument(new PdfReader(path));
+            StringBuilder text = new StringBuilder();
+            Assert.True(pdfDoc.GetNumberOfPages() > 0);
+            
+            if (pdfDoc.GetNumberOfPages() > 0)
             {
-                string[] files = System.IO.Directory.GetFiles(ChooseFilesFolder.SelectedPath);
-                Assert.NotEqual("C:\\Users\\James\\Desktop", ChooseFilesFolder.SelectedPath);
+                for (int i = 1; i <= pdfDoc.GetNumberOfPages(); i++)
+                {
+                    string fixedString = iText.Kernel.Pdf.Canvas.Parser.PdfTextExtractor.GetTextFromPage(pdfDoc.GetPage(i));
+                    text.Append(fixedString);
+                }
+                return text.ToString();
             }
+            
+            else return null;
+            
+        }
+    }
+    public class PdfFilesContentsAsString
+    {
+        public List<string> textFromFiles = new List<string>();
+        [Theory]
+        [InlineData("Z:\\chemistry\\2019-20\\Blackboard\\material chemistry essays\\01180527.pdf")]
+        public void SetText(string pageText)
+        {
+            textFromFiles.Add(pageText);
         }
     }
 }
--- Turnitin Exporter/Form1.cs
+++ Turnitin Exporter/Form1.cs
@@ -24,12 +24,14 @@ namespace Turnitin_Exporter
             var FilesToParse = ChooseFolder();
             Directory FilesInFolder = new Directory();
             FilesInFolder.SetFilesInFolderToDirectory(FilesToParse);
-            FilesText filesText = new FilesText();
+            FilesTextSubstring SubstringFromfilesText = new FilesTextSubstring();
             PDFParser pdfParser = new PDFParser();
-            for(int i = 0; i < FilesToParse.Length; i++)
-            { 
-            filesText.SetTextFromFiles(pdfParser.ExtractTextFromPdf(FilesInFolder.files[i]));
-            filesText.GetStudentIDAndFeedback();
+            PdfFilesContentsAsString OriginalText = new PdfFilesContentsAsString();
+            for (int i = 0; i < FilesToParse.Length; i++)
+            {
+                OriginalText.SetText(pdfParser.ExtractTextFromPdf(FilesInFolder.files[i]));
+                //SubstringFromfilesText.SetTextFromFiles(pdfParser.ExtractTextFromPdf(FilesInFolder.files[i]));
+                SubstringFromfilesText.GetStudentIDAndFeedback(OriginalText.textFromFiles);
             //filesText.WriteIDsToFile();
             //filesText.WriteFeedbackToFile();
             }
@@ -41,8 +43,7 @@ namespace Turnitin_Exporter
             DialogResult result = ChooseFilesFolder.ShowDialog();
             if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(ChooseFilesFolder.SelectedPath))
             {
-                //NewDirectoryObject(ChooseFilesFolder);
-                return System.IO.Directory.GetFiles(ChooseFilesFolder.SelectedPath);
+                return System.IO.Directory.GetFiles(ChooseFilesFolder.SelectedPath, "*.pdf");
             }
             return null;
         }
@@ -56,13 +57,21 @@ namespace Turnitin_Exporter
             files = filesToParse;
         }
     }
+    public class PdfFilesContentsAsString
+    {
+        public List<string> textFromFiles = new List<string>();
+        public void SetText(string pageText)
+        {
+            textFromFiles.Add(pageText);
+        }
+    }
     public class PDFParser
     {
         public string ExtractTextFromPdf(string path)
         {
 
             PdfDocument pdfDoc = new PdfDocument(new PdfReader(path));
-                StringBuilder text = new StringBuilder();
+            StringBuilder text = new StringBuilder();
             if (pdfDoc.GetNumberOfPages() > 0)
             {
                 //FilesText filesText = new FilesText();
@@ -76,37 +85,30 @@ namespace Turnitin_Exporter
             else return null;
         }
     }
-    public class FilesText
+    public class FilesTextSubstring
     {
-        public string teststring { get; set; }
-        public List<string> textFromFiles = new List<string>();
         public List<string> ID = new List<string>();
         public List<string> Feedback = new List<string>();
 
-        public void SetTextFromFiles(string files)
+        public void GetStudentIDAndFeedback(List<string> textList)
         {
-            teststring = files;
-            textFromFiles.Add(teststring);
-        }
-        public void GetStudentIDAndFeedback()
-        {
-            TextWriter tw = new StreamWriter(@"C:\Users\James\Documents\transferred files\IDs and Feedback\IDs and Feedback.txt");
-            for (int i = 0; i < textFromFiles.Count; i++)
+            //TextWriter tw = new StreamWriter(@"C:\Users\James\Documents\transferred files\IDs and Feedback\IDs and Feedback.txt");
+            for (int i = 0; i < textList.Count; i++)
             {
                 
-                int IDFrom = textFromFiles[i].IndexOf("SUBMISSION ID ") + "SUBMISSION ID ".Length;
-                int IDTo = textFromFiles[i].LastIndexOf(" CHARACTER");
-                ID.Add(textFromFiles[i].Substring(IDFrom, IDTo - IDFrom));
+                int IDFrom = textList[i].IndexOf("SUBMISSION ID ") + "SUBMISSION ID ".Length;
+                int IDTo = textList[i].LastIndexOf(" CHARACTER");
+                ID.Add(textList[i].Substring(IDFrom, IDTo - IDFrom));
                 Console.WriteLine(ID[i]);
-                tw.WriteLine(ID[i]);
-                int stringFrom = textFromFiles[i].IndexOf("Instructor") + "Instructor".Length;
-                int stringTo = textFromFiles[i].LastIndexOf("PAGE 1");
-                Feedback.Add(textFromFiles[i].Substring(stringFrom, stringTo - stringFrom));
+                //tw.WriteLine(ID[i]);
+                int stringFrom = textList[i].IndexOf("Instructor") + "Instructor".Length;
+                int stringTo = textList[i].LastIndexOf("PAGE 1");
+                Feedback.Add(textList[i].Substring(stringFrom, stringTo - stringFrom));
                 Console.WriteLine(Feedback[i]);
-                tw.WriteLine(Feedback[i]);
+                //tw.WriteLine(Feedback[i]);
                 //return result[i];
             }
-            tw.Close();
+            //tw.Close();
         }
         public void WriteIDsToFile()
         {
